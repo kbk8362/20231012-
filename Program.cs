caca6f46753392a451edfccab7fd51e20a236798
kbk8362 <kbk8362@naver.com>
2023-10-12 10:39:48 +0900

    초기 커밋.

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿# include <stdio.h>
+# include <stdlib.h>
+#define MAX_ELEMENT 200
+using System.Reflection.Metadata;
+typedef struct {
+    int key;
+} element;
+typedef struct {
+    element heap[MAX_ELEMENT];
+    int heap_size;
+} HeapType;
